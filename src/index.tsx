f28d74b8c520fe9261548bb054e1f7375187b569
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2020-02-19 17:46:51 +0900

    add router

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter, Route } from 'react-router-dom';
 import App from './App';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Index = () => {
+  return <a href="https://scrapbox.io/nishio/%E8%A6%8B%E3%82%84%E3%81%99%E3%81%84%E6%9D%A1%E6%96%87">see here</a>
+}
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Route path="/" exact children={<Index />} />
+    <Route path="/:law/:path" children={<App />} />
+  </BrowserRouter>,
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
